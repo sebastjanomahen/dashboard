f76fd5ce31b7cc94bd9d6819aa7bf8e2e843d0e9
Sebastjan O <sebaomahen@gmail.com>
2020-05-08 11:11:11 +0200

    Updates

---
+++ precache-manifest.d5c19e9e1b03476ab8cb69d832089166.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49d72b9c7f5f4252cc1",
+    "url": "/dashboard/static/css/main.2a9b46b3.chunk.css"
+  },
+  {
+    "revision": "c49d72b9c7f5f4252cc1",
+    "url": "/dashboard/static/js/main.c49d72b9.chunk.js"
+  },
+  {
+    "revision": "60e3e8346367182557e7",
+    "url": "/dashboard/static/css/1.91ed4522.chunk.css"
+  },
+  {
+    "revision": "60e3e8346367182557e7",
+    "url": "/dashboard/static/js/1.60e3e834.chunk.js"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/dashboard/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/dashboard/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/dashboard/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/dashboard/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/dashboard/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/dashboard/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/dashboard/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/dashboard/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/dashboard/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/dashboard/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/dashboard/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/dashboard/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "5e1d1e481ba6105335062c395d15ee57",
+    "url": "/dashboard/index.html"
+  }
+];
